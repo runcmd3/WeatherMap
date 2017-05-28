2f9a95ae44297434f3ee107147764dbea74d975b
David Argilan <argilanster@gmail.com>
2017-05-28 07:59:14 -0400

    Initial Commit

---
+++ WeatherMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherMap
+//
+//  Created by David Argilan on 5/28/17.
+//  Copyright © 2017 David Argilan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
